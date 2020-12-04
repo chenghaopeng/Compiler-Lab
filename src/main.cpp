5519002a87984d9822325934ab886ef4e85e26d7
tracer-compiler2020 <compilers@software.nju.edu.cb>
2020-12-04 16:10:02 +0800

    > make 181250020 chenghaopeng Linux ubuntu 5.4.0-53-generic #59-Ubuntu SMP Wed Oct 21 09:38:44 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:10:02 up 2 days,  3:46,  1 user,  load average: 0.26, 0.08, 0.02 6293c3a8abaa856973ccac61944ac971cb6d3bd

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,7 @@ extern int flag;
 extern char* buf;
 
 int main (int argc, char* argv[]) {
-    if (argc > 1) {
-        yyin = fopen(argv[1], "r");
-        yylex();
-        if (flag) fprintf(stderr, buf);
-    }
+    yylex();
+    if (flag) fprintf(stderr, buf);
     return 0;
 }
