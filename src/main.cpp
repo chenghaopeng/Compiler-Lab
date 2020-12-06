a46ec87862a1d5e8b151a13333eb0596461775ab
tracer-compiler2020 <compilers@software.nju.edu.cb>
2020-12-06 10:39:09 +0800

    > make 181250020 chenghaopeng Linux ubuntu 5.4.0-53-generic #59-Ubuntu SMP Wed Oct 21 09:38:44 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:39:09 up 2 days, 18:29,  1 user,  load average: 0.20, 0.05, 0.01 f5d1ce9028ca4353b03b013e1ad8aa999ff87d5c

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,47 @@
 #include <string>
-#include <stack>
+#include <vector>
 #include <iostream>
 using namespace std;
 
 extern "C" {
     void yyread(char*);
     int yyparse(void);
+    char* yytext;
 }
 
-stack<string> ss;
+struct AstNode {
+    string name;
+    int lineno;
+    string opt;
+    int lc, rc;
+};
 
-extern "C" {
-    void push_str(char *s) {
-        ss.push(string(s));
+vector<AstNode> nodes;
+
+int newAstNode (char* name, int num, int* args) {
+    AstNode node;
+    node.name = string(name);
+    if (num) {
+        int tmp = node.lc = args[0];
+        node.lineno = nodes[tmp].lineno;
+        for (int i = 1; i < num; ++i) {
+            nodes[tmp].rc = args[i];
+            tmp = nodes[tmp].rc;
+        }
     }
+    else {
+        node.lineno = args[0];
+        node.opt = string(yytext);
+    }
+    nodes.push_back(node);
+    return nodes.size() - 1;
 }
 
 int main (int argc, char* argv[]) {
     if (argc > 1) {
         yyread(argv[1]);
         yyparse();
-        while (!ss.empty()) {
-            cout << ss.top() << '\n';
-            ss.pop();
-        }
+        
     }
     return 0;
 }
