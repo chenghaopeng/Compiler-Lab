b78f02083a431713c00b89178225da59d6e64aec
tracer-compiler2020 <compilers@software.nju.edu.cb>
2020-12-06 13:34:37 +0800

    > make 181250020 chenghaopeng Linux ubuntu 5.4.0-53-generic #59-Ubuntu SMP Wed Oct 21 09:38:44 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:34:37 up 2 days, 19:11,  1 user,  load average: 0.08, 0.06, 0.02 123cac206cd5178112e298e0589b9b6f58ba0ca1

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <cstdarg>
 using namespace std;
 
 extern "C" {
@@ -20,20 +21,22 @@ vector<AstNode> nodes;
 
 extern "C"
 int newAstNode (char* name, int num, ...) {
+    va_list args;
+    va_start(args, num);
     AstNode node;
     node.name = string(name);
-    // if (num) {
-    //     int tmp = node.lc = args[0];
-    //     node.lineno = nodes[tmp].lineno;
-    //     for (int i = 1; i < num; ++i) {
-    //         nodes[tmp].rc = args[i];
-    //         tmp = nodes[tmp].rc;
-    //     }
-    // }
-    // else {
-    //     node.lineno = args[0];
-    //     node.opt = string(yytext);
-    // }
+    if (num) {
+        int tmp = node.lc = va_arg(args, int);
+        node.lineno = nodes[tmp].lineno;
+        for (int i = 1; i < num; ++i) {
+            nodes[tmp].rc = va_arg(args, int);
+            tmp = nodes[tmp].rc;
+        }
+    }
+    else {
+        node.lineno = va_arg(args, int);
+        node.opt = string(yytext);
+    }
     nodes.push_back(node);
     return nodes.size() - 1;
 }
@@ -42,7 +45,7 @@ int main (int argc, char* argv[]) {
     if (argc > 1) {
         yyread(argv[1]);
         yyparse();
-        cout << nodes.size();
+        cout << nodes[nodes.size() - 1].name;
     }
     return 0;
 }
