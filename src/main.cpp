874e48ca717b3ce509d36e97da2f4b5954ee7258
tracer-compiler2020 <compilers@software.nju.edu.cb>
2020-12-06 14:30:48 +0800

    > make 181250020 chenghaopeng Linux ubuntu 5.4.0-53-generic #59-Ubuntu SMP Wed Oct 21 09:38:44 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:30:48 up 2 days, 20:06,  1 user,  load average: 0.15, 0.05, 0.01 b0548a73a868f16564623d5689e2675c7166f74

--- src/main.cpp
+++ src/main.cpp
@@ -43,13 +43,61 @@ int newAstNode (char* name, int num, ...) {
     return nodes.size() - 1;
 }
 
+void print_number (string number) {
+    size_t dot = number.find('.');
+    if (dot == string::npos) {
+        int n = 0;
+        if (number[0] == '0') {
+            if (number.size() == 1) n = 0;
+            else if (number[1] == 'x' || number[1] == 'X') {
+                for (int i = 2; i < number.size(); ++i) {
+                    int t = 0;
+                    if (number[i] >= '0' && number[i] <= '9') t = number[i] - '0';
+                    if (number[i] >= 'a' && number[i] <= 'z') t = number[i] - 'a' + 10;
+                    if (number[i] >= 'A' && number[i] <= 'Z') t = number[i] - 'A' + 10;
+                    n = n * 16 + t;
+                }
+            }
+            else {
+                for (int i = 1; i < number.size(); ++i) {
+                    n = n * 8 + number[i] - '0';
+                }
+            }
+        }
+        else {
+            for (int i = 0; i < number.size(); ++i) {
+                n = n * 10 + number[i] - '0';
+            }
+        }
+        cout << n;
+    }
+    else {
+        double f = 0, a = 0, b = 0;
+        int i, j = 0;
+        for (i = 0; number[i] != '.'; ++i) a = a * 10 + number[i] - '0';
+        for (i++; i < number.size() && number[i] != 'e'; ++i, ++j) b = b * 10 + number[i] - '0';
+        while (j--) b /= 10;
+        f = a + b;
+        if (i < number.size()) {
+            int n = atoi(number.c_str() + i + 1);
+            if (n > 0) for (int i = 0; i < n; ++i) f *= 10;
+            if (n < 0) for (int i = 0; i < -n; ++i) f /= 10;
+        }
+        cout << f;
+    }
+}
+
 void print (int u, int dep) {
     if (u < 0) return;
     if (nodes[u].lineno != -1) {
         for (int i = 0; i < dep; ++i) cout << "  ";
         cout << nodes[u].name;
-        if (nodes[u].name == "ID" || nodes[u].name == "TYPE" || nodes[u].name == "INT" || nodes[u].name == "FLOAT") cout << ": " << nodes[u].opt;
         if (nodes[u].name[1] >= 'a' && nodes[u].name[1] <= 'z') cout << " (" << nodes[u].lineno << ")";
+        else {
+            cout << ": ";
+            if (nodes[u].name == "ID" || nodes[u].name == "TYPE") cout << nodes[u].opt;
+            else if (nodes[u].name == "INT" || nodes[u].name == "FLOAT") print_number(nodes[u].opt);
+        }
         cout << "\n";
     }
     print(nodes[u].lc, dep + 1);
