6b5340f7619f793be18efc8796935f93202f20b6
tracer-compiler2020 <compilers@software.nju.edu.cb>
2020-12-09 10:30:11 +0800

    > make 181250020 chenghaopeng Linux ubuntu 5.4.0-53-generic #59-Ubuntu SMP Wed Oct 21 09:38:44 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:30:11 up  9:50,  1 user,  load average: 0.28, 0.25, 0.21 ae116d182af5eb9938f2d884e4444c8a4d93549d

--- src/main.cpp
+++ src/main.cpp
@@ -113,7 +113,8 @@ int main (int argc, char* argv[]) {
     if (argc > 1) {
         yyread(argv[1]);
         yyparse();
-        if (flag) print(nodes.size() - 1, 0);
+        // if (flag)
+        print(nodes.size() - 1, 0);
     }
     return 0;
 }
