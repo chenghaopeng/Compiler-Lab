34d68615a324b6dfbd7de21ce502eae1e076de23
tracer-compiler2020 <compilers@software.nju.edu.cb>
2020-12-24 17:50:48 +0800

    > make 181250020 chenghaopeng Linux ubuntu 5.4.0-56-generic #62-Ubuntu SMP Mon Nov 23 19:20:19 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:50:48 up 1 day, 10:30,  1 user,  load average: 0.23, 0.24, 0.19 21e9c2b417acd9649a4b072bf5b9a9be7303afb0

--- src/semantic.cpp
+++ src/semantic.cpp
@@ -1,6 +1,31 @@
 #include "global.h"
 #include <vector>
+#include <iostream>
+using namespace std;
 
-void semantic (vector<AstNode> *nodes) {
-    
+vector<AstNode>* nodes;
+
+AstNode get (int i) {
+    return (*nodes)[i];
+}
+
+string getProduction (int u) {
+    int x = get(u).lc;
+    if (x == -1) return "";
+    string production = get(x).name;
+    for (x = get(x).rc; x != -1; x = get(x).rc) {
+        production += " " + get(x).name;
+    }
+    return production;
+}
+
+void analyseProgram (int u) {
+    AstNode program = get(u);
+    string production = getProduction(u);
+    cout << production << '\n';
+}
+
+void semantic (vector<AstNode>* _nodes) {
+    nodes = _nodes;
+    analyseProgram(nodes->size() - 1);
 }
