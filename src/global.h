a25f32da62512e3d220c8460fb65e0549b8d8440
tracer-compiler2020 <compilers@software.nju.edu.cb>
2021-01-16 12:32:42 +0800

    > make 181250020 chenghaopeng Linux ubuntu 5.4.0-58-generic #64-Ubuntu SMP Wed Dec 9 08:16:25 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:32:42 up  5:24,  1 user,  load average: 0.08, 0.02, 0.01 b5b0820f7264ed1dd62e5ab9c4d7577039159524

--- src/global.h
+++ src/global.h
@@ -13,6 +13,86 @@ struct AstNode {
     AstNode () { name = opt = ""; lineno = lc = rc = -1; }
 };
 
+struct Type;
+
+enum TypeKind {
+    BASIC,
+    ARRAY,
+    STRUCTURE
+};
+
+enum ValueType {
+    LEFT,
+    RIGHT
+};
+
+enum BasicType {
+    INT,
+    FLOAT
+};
+
+struct ArrayType {
+    Type* type;
+    int size;
+};
+
+struct Field {
+    Type* type;
+    string name;
+    Field* next;
+    Field () {
+        next = nullptr;
+    }
+};
+
+struct StructureType {
+    Field* field;
+};
+
+struct Type {
+    TypeKind kind;
+    ValueType valueType;
+    union {
+        BasicType basic;
+        ArrayType array;
+        StructureType structure;
+    };
+    Type () {}
+};
+
+enum SymbolKind {
+    VAR,
+    FIELD,
+    STRUCT,
+    FUNC
+};
+
+struct FunctionParameter {
+    Type* type;
+    FunctionParameter* next;
+    FunctionParameter () {
+        next = nullptr;
+    }
+};
+
+struct FunctionDeclaim {
+    Type* type;
+    FunctionParameter* parameter;
+};
+
+struct Symbol {
+    SymbolKind kind;
+    string name;
+    union {
+        Type* type;
+        FunctionDeclaim* declaim;
+    };
+    Symbol* next;
+    Symbol () {
+        next = nullptr;
+    }
+};
+
 AstNode get (int);
 string getProduction (int);
 vector<int> getSons (int);
