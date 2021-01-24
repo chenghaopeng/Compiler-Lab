912b5433ce9d90517d5a9b7a4903418f145869f2
tracer-compiler2020 <compilers@software.nju.edu.cb>
2021-01-24 22:45:38 +0800

    > make 181250020 chenghaopeng Linux ubuntu 5.8.0-38-generic #43~20.04.1-Ubuntu SMP Tue Jan 12 16:39:47 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  22:45:38 up 33 min,  1 user,  load average: 0.19, 0.23, 0.19 21d6e6bc18696b675442395350dc56491cecc19

--- src/assembly.cpp
+++ src/assembly.cpp
@@ -1,12 +1,15 @@
 #include "global.h"
 #include <map>
 #include <iostream>
+#include <set>
 
-#define add outs.push_back
-#define insert(a) outs.insert(outs.begin() + 3, a)
+#define oadd outs.push_back
+#define oinsert(a) outs.insert(outs.begin() + 3, a)
 
 vector<string> outs;
 map<string, bool> inData;
+set<string> vars;
+vector<string> varstmp;
 
 int regc = 0;
 
@@ -15,13 +18,14 @@ string newReg () {
 }
 
 string mksur (string var, string reg) {
+    vars.insert(var);
     if (!inData[var]) {
         inData[var] = true;
-        insert(var + ": .word 0");
+        oinsert(var + ": .word 0");
     }
     string tmp = newReg();
-    add("la " + tmp + ", " + var);
-    add("lw " + reg + ", 0(" + tmp + ")");
+    oadd("la " + tmp + ", " + var);
+    oadd("lw " + reg + ", 0(" + tmp + ")");
     return reg;
 }
 
@@ -32,18 +36,18 @@ string mksur (Operand* operand) {
         mksur(irGetVariable(operand->var), reg);
         break;
     case CONSTANT:
-        add("li " + reg + ", " + to_string(operand->constVal));
+        oadd("li " + reg + ", " + to_string(operand->constVal));
         break;
     case REF:
         var = irGetVariable(operand->ref->var);
-        add("la " + reg + ", " + var);
+        oadd("la " + reg + ", " + var);
         break;
     case DEREF:
         var = irGetVariable(operand->deref->var);
         tmp = newReg();
-        add("la " + tmp + ", " + var);
-        add("lw " + reg + ", 0(" + tmp + ")");
-        add("lw " + reg + ", 0(" + reg + ")");
+        oadd("la " + tmp + ", " + var);
+        oadd("lw " + reg + ", 0(" + tmp + ")");
+        oadd("lw " + reg + ", 0(" + reg + ")");
         break;
     default:
         break;
@@ -57,8 +61,8 @@ void save (Operand* operand, string reg) {
     case VARIABLE:
         var = irGetVariable(operand->var);
         tmp = newReg();
-        add("la " + tmp + ", " + var);
-        add("sw " + reg + ", 0(" + tmp + ")");
+        oadd("la " + tmp + ", " + var);
+        oadd("sw " + reg + ", 0(" + tmp + ")");
         break;
     case CONSTANT:
         break;
@@ -67,9 +71,9 @@ void save (Operand* operand, string reg) {
     case DEREF:
         var = irGetVariable(operand->deref->var);
         tmp = newReg();
-        add("la " + tmp + ", " + var);
-        add("lw " + tmp + ", 0(" + tmp + ")");
-        add("sw " + reg + ", 0(" + tmp + ")");
+        oadd("la " + tmp + ", " + var);
+        oadd("lw " + tmp + ", 0(" + tmp + ")");
+        oadd("sw " + reg + ", 0(" + tmp + ")");
         break;
     default:
         break;
@@ -78,8 +82,8 @@ void save (Operand* operand, string reg) {
 
 void save (string var, string reg) {
     string tmp = newReg();
-    add("la " + tmp + ", " + var);
-    add("sw " + reg + ", 0(" + tmp + ")");
+    oadd("la " + tmp + ", " + var);
+    oadd("sw " + reg + ", 0(" + tmp + ")");
 }
 
 string getLabel (int id) {
@@ -91,121 +95,146 @@ string getFunctionName (string name) {
 }
 
 void asmPrint (FILE* fp) {
-    add(".data");
-    add("_prompt: .asciiz \"Enter an integer:\"");
-    add("_ret: .asciiz \"\\n\"");
-    add(".globl main");
-    add(".text");
-    add("read:\n  li $v0, 4\n  la $a0, _prompt\n  syscall\n  li $v0, 5\n  syscall\n  jr $ra\n\nwrite:\n  li $v0, 1\n  syscall\n  li $v0, 4\n  la $a0, _ret\n  syscall\n  move $v0, $0\n  jr $ra\n");
+    oadd(".data");
+    oadd("_prompt: .asciiz \"Enter an integer:\"");
+    oadd("_ret: .asciiz \"\\n\"");
+    oadd("_function_stack: .space 65536");
+    oadd(".globl main");
+    oadd(".text");
+    oadd("read:\n  li $v0, 4\n  la $a0, _prompt\n  syscall\n  li $v0, 5\n  syscall\n  jr $ra\n\nwrite:\n  li $v0, 1\n  syscall\n  li $v0, 4\n  la $a0, _ret\n  syscall\n  move $v0, $0\n  jr $ra\n");
     string r1, r2, r3;
     for (int i = 0; i < irCount(); ++i) {
         InterRepresentation* ir = irGet(i);
         regc = 8;
         switch (ir->kind) {
         case LABEL:
-            add(getLabel(ir->labelId) + ":");
+            oadd(getLabel(ir->labelId) + ":");
             break;
         case FUNCTION:
-            add(getFunctionName(ir->functionName) + ":");
+            oadd(getFunctionName(ir->functionName) + ":");
+            vars = set<string>();
+            if (ir->functionName == "main") {
+                oadd("la $2, _function_stack");
+            }
             break;
         case ASSIGN:
             r1 = mksur(ir->assign.left);
             r2 = mksur(ir->assign.right);
-            add("move " + r1 + ", " + r2);
+            oadd("move " + r1 + ", " + r2);
             save(ir->assign.left, r1);
             break;
         case ADD:
             r1 = mksur(ir->binaryAssign.left);
             r2 = mksur(ir->binaryAssign.op1);
             r3 = mksur(ir->binaryAssign.op2);
-            add("add " + r1 + ", " + r2 + ", " + r3);
+            oadd("add " + r1 + ", " + r2 + ", " + r3);
             save(ir->binaryAssign.left, r1);
             break;
         case SUB:
             r1 = mksur(ir->binaryAssign.left);
             r2 = mksur(ir->binaryAssign.op1);
             r3 = mksur(ir->binaryAssign.op2);
-            add("sub " + r1 + ", " + r2 + ", " + r3);
+            oadd("sub " + r1 + ", " + r2 + ", " + r3);
             save(ir->binaryAssign.left, r1);
             break;
         case DIV:
             r1 = mksur(ir->binaryAssign.left);
             r2 = mksur(ir->binaryAssign.op1);
             r3 = mksur(ir->binaryAssign.op2);
-            add("div " + r2 + ", " + r3);
-            add("mflo " + r1);
+            oadd("div " + r2 + ", " + r3);
+            oadd("mflo " + r1);
             save(ir->binaryAssign.left, r1);
             break;
         case MUL:
             r1 = mksur(ir->binaryAssign.left);
             r2 = mksur(ir->binaryAssign.op1);
             r3 = mksur(ir->binaryAssign.op2);
-            add("mul " + r1 + ", " + r2 + ", " + r3);
+            oadd("mul " + r1 + ", " + r2 + ", " + r3);
             save(ir->binaryAssign.left, r1);
             break;
         case GOTO:
-            add("j " + getLabel(ir->gotoId));
+            oadd("j " + getLabel(ir->gotoId));
             break;
         case IF_GOTO:
             r1 = mksur(ir->if_goto.left);
             r2 = mksur(ir->if_goto.right);
             r3 = asmGetRelop(ir->if_goto.kind);
-            add(r3 + " " + r1 + ", " + r2 + ", " + getLabel(ir->if_goto.gotoId));
+            oadd(r3 + " " + r1 + ", " + r2 + ", " + getLabel(ir->if_goto.gotoId));
             break;
         case RETURN:
             r1 = mksur(ir->returnVal);
-            add("move $2, " + r1);
-            add("jr $ra\n");
+            oadd("move $2, " + r1);
+            oadd("jr $ra\n");
             break;
         case DEC:
             r1 = irGetVariable(ir->dec.var);
             inData[r1] = true;
-            insert(r1 + ": .space " + to_string(ir->dec.size));
+            oinsert(r1 + ": .space " + to_string(ir->dec.size));
             break;
         case ARG:
             r1 = mksur(ir->arg);
-            add("addi $sp, $sp, -4");
-            add("sw " + r1 + ", 0($sp)");
+            oadd("addi $sp, $sp, -4");
+            oadd("sw " + r1 + ", 0($sp)");
             break;
         case CALL:
-            add("addi $sp, $sp, -4");
-            add("sw $ra, 0($sp)");
-            add("jal " + getFunctionName(ir->call.functionName));
-            add("lw $ra, 0($sp)");
-            add("addi $sp, $sp, 4");
+            varstmp = vector<string>();
+            r1 = newReg();
+            for (set<string>::iterator it = vars.begin(); it != vars.end(); ++it) {
+                r1 = mksur(*it, r1);
+                oadd("sw " + r1 + ", 0($3)");
+                oadd("addi $3, $3, 4");
+                regc--;
+                varstmp.push_back(*it);
+            }
+            regc--;
+            oadd("addi $sp, $sp, -4");
+            oadd("sw $ra, 0($sp)");
+            oadd("jal " + getFunctionName(ir->call.functionName));
+            oadd("lw $ra, 0($sp)");
+            oadd("addi $sp, $sp, 4");
+            r1 = newReg();
+            for (int i = varstmp.size() - 1; i >= 0; --i) {
+                r1 = mksur(varstmp[i], r1);
+                oadd("addi $3, $3, -4");
+                oadd("lw " + r1 + ", 0($3)");
+                save(varstmp[i], r1);
+                regc--;
+                regc--;
+            }
+            regc--;
             r1 = mksur(irGetVariable(ir->call.ret), newReg());
-            add("move " + r1 + ", $2");
+            oadd("move " + r1 + ", $2");
             save(irGetVariable(ir->call.ret), r1);
             break;
         case PARAM:
             r1 = mksur(irGetVariable(ir->param), newReg());
             r2 = newReg();
-            add("lw " + r2 + ", 0($sp)");
-            add("addi $sp, $sp, 4");
-            add("lw " + r1 + ", 0($sp)");
-            add("addi $sp, $sp, 4");
-            add("addi $sp, $sp, -4");
-            add("sw " + r2 + ", 0($sp)");
+            oadd("lw " + r2 + ", 0($sp)");
+            oadd("addi $sp, $sp, 4");
+            oadd("lw " + r1 + ", 0($sp)");
+            oadd("addi $sp, $sp, 4");
+            oadd("addi $sp, $sp, -4");
+            oadd("sw " + r2 + ", 0($sp)");
             save(irGetVariable(ir->param), r1);
             break;
         case READ:
-            add("addi $sp, $sp, -4");
-            add("sw $ra, 0($sp)");
-            add("jal read");
-            add("lw $ra, 0($sp)");
-            add("addi $sp, $sp, 4");
+            oadd("addi $sp, $sp, -4");
+            oadd("sw $ra, 0($sp)");
+            oadd("jal read");
+            oadd("lw $ra, 0($sp)");
+            oadd("addi $sp, $sp, 4");
             r1 = mksur(ir->rw);
-            add("move " + r1 + ", $v0");
+            oadd("move " + r1 + ", $v0");
             save(irGetVariable(ir->rw->var), r1);
             break;
         case WRITE:
             r1 = mksur(ir->rw);
-            add("move $a0, " + r1);
-            add("addi $sp, $sp, -4");
-            add("sw $ra, 0($sp)");
-            add("jal write");
-            add("lw $ra, 0($sp)");
-            add("addi $sp, $sp, 4");
+            oadd("move $a0, " + r1);
+            oadd("addi $sp, $sp, -4");
+            oadd("sw $ra, 0($sp)");
+            oadd("jal write");
+            oadd("lw $ra, 0($sp)");
+            oadd("addi $sp, $sp, 4");
             break;
         default:
             break;
