a54d1c93460f466177bc0da6a1fe381590d2da6b
tracer-compiler2020 <compilers@software.nju.edu.cb>
2021-01-25 12:38:07 +0800

    > make 181250020 chenghaopeng Linux ubuntu 5.8.0-38-generic #43~20.04.1-Ubuntu SMP Tue Jan 12 16:39:47 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  12:38:07 up  2:47,  1 user,  load average: 0.56, 0.29, 0.31 ff66e93eb707036b3c117456ec165e0c7e68cd62

--- src/assembly.cpp
+++ src/assembly.cpp
@@ -100,7 +100,7 @@ void asmPrint (FILE* fp) {
     oadd(".data");
     oadd("_prompt: .asciiz \"Enter an integer:\"");
     oadd("_ret: .asciiz \"\\n\"");
-    oadd("_function_stack: .space 524280");
+    oadd("_function_stack: .space 1073741824");
     oadd(".globl main");
     oadd(".text");
     oadd("read:\n  li $v0, 4\n  la $a0, _prompt\n  syscall\n  li $v0, 5\n  syscall\n  jr $ra\n\nwrite:\n  li $v0, 1\n  syscall\n  li $v0, 4\n  la $a0, _ret\n  syscall\n  move $v0, $0\n  jr $ra\n");
